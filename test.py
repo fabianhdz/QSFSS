bd16f30edf0459b4ed2e71b13476b6abb2b9e8ad
Fabian Hernandez <fabian.hernandezone@gmail.com>
2025-04-24 19:22:43 -0500

    Add decrypt

--- test.py
+++ test.py
@@ -9,6 +9,16 @@ print("ek:", ek)
 print("dk:", dk)
 
 m = token_bytes(32)
+print("m:", m)
 r = token_bytes(32)
 c = kpke.encrypt(ek, m, r)
 print("ciphertext:", c)
+
+
+m_prime = kpke.decrypt(dk, c)
+
+if m == m_prime:
+    print("m_prime:", m_prime)
+    print("Decryption successful, m == m'")
+else:
+    print("Decryption failed, m != m'")
