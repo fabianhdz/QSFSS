03773be4a71dc80664d8706a3f81ae396458360f
ZeroNeji <jose.moreno18@utrgv.edu>
2025-04-26 16:36:38 -0500

    Made a new branch since the old one had a conflict with requirements
    The same as the old one but decrypt now catches the invalidtag error when the decryption is incorrect.

---
+++ AES_GCM.py
@@ -0,0 +1,28 @@
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from cryptography.exceptions import InvalidTag
+import os
+
+class aesgcm:
+  def __init__(self, key):
+    self.key = key[:32]
+
+  def changekey(self, key):
+    self.key = key[:32]
+
+  def encrypt(self, data):
+    iv = os.urandom(12)
+    cipher = Cipher(algorithms.AES(self.key), modes.GCM(iv), backend=default_backend())
+    encryptor = cipher.encryptor()
+    ciphertext = encryptor.update(data) + encryptor.finalize()
+    tag = encryptor.tag
+    return iv, ciphertext, tag
+
+  def decrypt(self, iv, ciphertext, tag,):
+    cipher = Cipher(algorithms.AES(self.key), modes.GCM(iv, tag), backend=default_backend())
+    decryptor = cipher.decryptor()
+    try:
+      plaintext = decryptor.update(ciphertext) + decryptor.finalize()
+      return plaintext.decode()
+    except InvalidTag:
+      return "Invalid decryption attempt"
